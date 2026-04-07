ee17baceef0c0794aed068cecf6741362a8de0b4
Filip Bergander <filip.bergander1996@gmail.com>
2026-04-07 15:11:06 +0200

    Lagt till formulär med knapp samt lite styling. Utökat tidigare kod inom class.ts, laborerar med hur jag ska få till det.

---
+++ src/addTodo.ts
@@ -0,0 +1,14 @@
+// Importerar interfacet Todo
+import { Todo } from './interface';
+
+export class addTodo implements Todo {
+    task: string;
+    completed: boolean;
+    priority: number;
+
+    constructor(task: string, completed: boolean, priority: number) {
+        this.task = task;
+        this.completed = completed;
+        this.priority = priority;
+    }
+}
--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,6 @@
 // Importerar interfacet Todo
 import { Todo } from './interface';
+import { addTodo } from './addTodo';
 
 export class TodoList implements Todo {
     task: string;
@@ -14,29 +15,43 @@ export class TodoList implements Todo {
         this.loadFromLocalStorage();
     }
 
+    // När användaren lägger till en ny att göra uppgift
     addTodo(task: string, priority: number): boolean {
-        if (task.length > 0 && (priority === )) {
-            this.todos.push(newTodo);
+        if (task.length > 0 && ((priority === 1) || (priority === 2) || (priority === 3))) {
+            this.todos.push({ task, completed: false, priority });
             this.saveToLocalStorage();
             return true;
-        } else return false;
+        } else {
+            return false;
+        }
     }
 
-    markTodoCompleted(todoIndex: number): void {
-
+    // När användaren markerar en uppgift som utförd
+    public markTodoCompleted(todoIndex: number): void {
+        this.todos = this.todos.filter(t => t.task !== this.todos[todoIndex].task);
+        this.saveToLocalStorage();
     }
+
     // För att hämta todos-arrayen
     getTodos(): Todo[] {
-        return this.todos;
+        const storedTodo = localStorage.getItem("todos-key");
+        if (storedTodo) {
+            const rawTodo = JSON.parse(storedTodo); // Om todos finns lagrade i localStorage, returneras de som en array
+            return rawTodo.map((t: { task: string; completed: boolean; priority: number; }) =>
+                new Todo(t.task, t.completed, t.priority));
+        } else {
+            return []; // Om inga todos finns lagrade i localStorage, returneras en tom array
+        }
     }
 
     // För att spara todos-arrayen inom localStorage 
-    saveToLocalStorage(): void {
-        localStorage.setItem("todos", JSON.stringify(this.todos))
+    public saveToLocalStorage(): void {
+        localStorage.setItem("todos-key", JSON.stringify(this.todos))
     }
+
     // För att ladda todos-array från localStorage
     loadFromLocalStorage(): void {
-        const storedTodo = localStorage.getItem("todos");
+        const storedTodo = localStorage.getItem("todos-key");
         if (storedTodo) {
             this.todos = JSON.parse(storedTodo);
         } else return;
